df392a251c7f12e098a1ab162be796471e4d955b
Bashar <87953302+Beshoo-o@users.noreply.github.com>
2024-08-14 17:55:03 +0300

    first commit

---
+++ Config.js
@@ -0,0 +1,22 @@
+/**
+ * Made By RSS Store
+ * ! https://discord.gg/prw
+*/
+
+const Config = {};
+
+Config.Token = "Bot token | توكن البوت"; // توكن البوت
+
+Config.Channels = [
+  // Put the channel id and emoji here | حط ايدي الروم و الايموجي هنا
+  {
+    ID: "Channel id | ايدي الروم", // Channel id | ايدي الروم
+    Reactions: ["Emoji | الايموجي", "Emoji | الايموجي", "Emoji | الايموجي"], // Emoji | الايموجي
+  },
+  {
+    ID: "Channel id | ايدي الروم", // Channel id | ايدي الروم
+    Reactions: ["Emoji | الايموجي", "Emoji | الايموجي", "Emoji | الايموجي"], // Emoji | الايموجي
+  },
+];
+
+module.exports = Config
---
+++ README.md
@@ -0,0 +1,15 @@
+## Hi this is a Auto Reaction Discord Bot
+
+## It was code by Js Language.
+
+## How to Use
+
+**Getting Started**:
+- Install [NodeJs](https://nodejs.org/en)
+- Extract the files
+- Open the console
+- Type ```bach npm install```
+- Configure the Config.js
+- Run the bot by clicking Start.cmd
+
+### If you face any issues please contact me at [Discord](https://discord.gg/VzSHHHAsTG)
